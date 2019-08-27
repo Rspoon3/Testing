068265cb54260cfdb0c720411d00012007183e7e
Ricky <Richardwitherspoon3@gmail.com>
2019-08-27 09:29:37 -0400

    Initial Commit

---
+++ Testing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing
+//
+//  Created by Richard Witherspoon on 8/27/19.
+//  Copyright © 2019 Richard Witherspoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
