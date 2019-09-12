15f2420e1716cc947224c1a8687e8bcb225a4e4f
Ricky <Richardwitherspoon3@gmail.com>
2019-09-11 20:09:03 -0400

    screen background color

--- Testing/ContentView.swift
+++ Testing/ContentView.swift
@@ -11,49 +11,45 @@ import SwiftUI
 import SwiftUI
 import Combine
 
-struct ID : Identifiable{
+struct Person : Identifiable{
     var id : Int
+    var name : String
 }
 
 struct ContentView: View {
+    let people = [
+        Person(id: 1, name: "Ricky"),
+        Person(id: 2, name: "Dani"),
+        Person(id: 3, name: "Mark"),
+        Person(id: 4, name: "Kailin"),
+        Person(id: 5, name: "James"),
+        Person(id: 5, name: "Jenna")
+    ]
     
     var body: some View {
-        ScrollView{
-            MyList()
-            MyList()
-            MyList()
-        }
-    }
-}
-
-struct MyList : View {
-    let ids = [ID(id: 1),ID(id: 2),ID(id: 3),ID(id: 4),ID(id: 5),ID(id: 6)]
-    
-    var body: some View{
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(alignment: .top, spacing: 100) {
-                ForEach(ids) {id in
-                    NavigationLink(
-                        destination: Text("daf")
-                    ){
-                        MyButton()
+        NavigationView{
+            ScrollView{
+                ZStack {
+                    Color.init(UIColor.red)
+                    VStack {
+                        ForEach(people, id: \.id) { person in
+                            Text(person.name)
+                                .frame(width: 300, height: 400)
+                                .background(Color.blue)
+                                .padding()
+                        }
                     }
                 }
-            }
-        }.frame(height: 50)
+            }.navigationBarTitle("Home")
+        }
+    }
+    
+    init(){
+        UINavigationBar.appearance().backgroundColor = .red
     }
 }
 
-struct MyButton : View {
-    @State var isYellow = true
-    
-    var body : some View{
-        Image(systemName: "star.fill")
-            .font(.title)
-            .foregroundColor(isYellow ? .yellow : .blue)
-            .frame(width: 50, height: 50)
-    }
-}
+
 
 
 #if DEBUG
