05e4b70067c39105f9cd5ea7ce852bf13357901b
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2020-02-11 16:22:11 -0500

    Pencilkit now works

--- Testing/ContentView.swift
+++ Testing/ContentView.swift
@@ -7,11 +7,19 @@
 //
 
 import SwiftUI
-import Vision
+import PencilKit
 
 struct ContentView : View {
+    @State var color = UIColor.black
+    @State var clear = false
     var body : some View{
-        Text("Hello, World")
+        Image("motorcycle")
+            .resizable()
+            .scaledToFit()
+            .overlay(
+                PKCanvas(color: $color, clear: $clear)
+        )
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -22,3 +30,44 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
+struct PKCanvas: UIViewRepresentable {
+    class Coordinator: NSObject, PKCanvasViewDelegate {
+        var pkCanvas: PKCanvas
+        
+        init(_ pkCanvas: PKCanvas) {
+            self.pkCanvas = pkCanvas
+        }
+    }
+    
+    @Binding var color:UIColor
+    @Binding var clear:Bool
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        let canvasView = PKCanvasView()
+        canvasView.tool = PKInkingTool(.pen, color: color, width: 10)
+        canvasView.isRulerActive = true
+        canvasView.delegate = context.coordinator
+        canvasView.isOpaque = false
+        canvasView.backgroundColor = .clear
+        canvasView.overrideUserInterfaceStyle = .light
+        
+        if let window = UIApplication.shared.windows.last, let toolPicker = PKToolPicker.shared(for: window) {
+            toolPicker.setVisible(true, forFirstResponder: canvasView)
+            toolPicker.addObserver(canvasView)
+            canvasView.becomeFirstResponder()
+        }
+        
+        return canvasView
+    }
+    
+    func updateUIView(_ canvasView: PKCanvasView, context: Context) {
+        if clear != context.coordinator.pkCanvas.clear{
+            canvasView.drawing = PKDrawing()
+        }
+        canvasView.tool = PKInkingTool(.pen, color: color, width: 10)
+    }
+}
