70c4ef9f960c8035a09bb15420e96d1a8caf6ab1
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2020-06-22 18:28:08 -0400

    updated for iOS 14

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Testing
+//
+//  Created by Richard Witherspoon on 8/27/19.
+//  Copyright © 2019 Richard Witherspoon. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App{
+    var body:some Scene{
+        WindowGroup{
+            ContentView()
+        }
+    }
+}
