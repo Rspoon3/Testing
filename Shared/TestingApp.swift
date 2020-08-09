472923638bcc41326d48c343fc31c098cafc043f
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2020-08-09 19:22:29 -0400

    Initial Commit

---
+++ Shared/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Shared
+//
+//  Created by Richard Witherspoon on 8/9/20.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
