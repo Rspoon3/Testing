715698293a45f5f27dad01d9a4c8664f18ee532e
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2020-09-24 10:45:58 -0400

    Working on pinned conversations

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -5,12 +5,66 @@
 //  Created by Richard Witherspoon on 8/9/20.
 //
 
+
 import SwiftUI
+import Combine
+
+struct Item: Identifiable, Equatable{
+    let id = UUID()
+    let value: Int
+    var isPinned = false
+}
 
 struct ContentView: View {
+    @State var items = Array(0...12).map({Item(value: $0)})
+    @Namespace private var animation
+    let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            LazyVGrid(columns: [
+                GridItem(.flexible()),
+                GridItem(.flexible()),
+                GridItem(.flexible()),
+            ], spacing: 20){
+                Section{
+                    ForEach(items.filter{$0.isPinned}){ item in
+                        Circle()
+                            .frame(height: 100)
+                            .overlay(Text(item.value.description).foregroundColor(.white))
+                            .onTapGesture{
+                                toggle(item)
+                            }
+                            .matchedGeometryEffect(id: item.id, in: animation)
+                    }
+                }
+            }.animation(.default)
+            
+            LazyVStack{
+                ForEach(items.filter{!$0.isPinned}){ item in
+                    Color.blue
+                        .clipShape(Circle())
+                        .frame(height: 100)
+                        .overlay(Text(item.value.description).foregroundColor(.white))
+                        .onTapGesture{
+                            toggle(item)
+                        }
+                        .matchedGeometryEffect(id: item.id, in: animation)
+                    
+                }
+            }
+            .animation(.default)
+            .onReceive(timer) {_ in
+                toggle(items.randomElement()!)
+            }
+        }
+    }
+    
+    private func toggle(_ item: Item){
+        if let index = items.firstIndex(where: {$0 == item}){
+            items[index].isPinned.toggle()
+        }
     }
 }
 
@@ -19,3 +73,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
