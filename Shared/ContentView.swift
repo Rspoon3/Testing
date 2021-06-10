3423a6dccb5e03671150f3ad430df559c71bf9b5
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-06-10 12:38:03 -0400

    • Added code example

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
+import CoreNFC
+
+class ContentVM: NSObject, ObservableObject, NFCNDEFReaderSessionDelegate{
+    var session: NFCNDEFReaderSession?
+    
+    func start() {
+        session = NFCNDEFReaderSession(delegate: self, queue: DispatchQueue.main, invalidateAfterFirstRead: false)
+        session?.alertMessage = "Hold your iPhone near the item to learn more about it."
+        session?.begin()
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for record in message.records {
+                if let string = String(data: record.payload, encoding: .ascii) {
+                    print(string)
+                }
+            }
+        }
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+}
 
 struct ContentView: View {
+    @StateObject var model = ContentVM()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Scan NFC"){
+            model.start()
+        }
     }
 }
 
