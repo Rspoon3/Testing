f1a8ae3c52bef979dc85a364fa8c2e5c87fd2b0c
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-01-04 10:47:47 -0500

    added debug mode

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,26 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var model = CarouselRepresentableViewModel()
+    @State private var debug = false
     private let shadowBottomPadding: CGFloat = 20
+    private let spacing: CGFloat = 12
     
     var body: some View {
-        ScrollView {
-            Text("TOP")
-            Text("Index: \(model.currentIndex.item)")
-            CarouselRepresentable(model: model)
-                .frame(height: model.height + shadowBottomPadding)
-                .onAppear{
-                    model.scrollToMiddle()
-//                    model.startTimer()
-                    model.configureScrollObserver()
+        NavigationView {
+            ZStack {
+                if debug {
+                    HStack(spacing: 0) {
+                        Color.blue
+                            .frame(width: spacing)
+                        Color.red
+                            .frame(width: spacing)
+                        Spacer()
+                        Color.red
+                            .frame(width: spacing)
+                        Color.blue
+                            .frame(width: spacing)
+                    }
                 }
-            Text("Bottom")
-            
-            ForEach(0..<100) {_ in
-                Color.orange
-                    .frame(height: 100)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
+                
+                ScrollView {
+                    Text("TOP")
+                    Text("Index: \(model.currentIndex.item)")
+                    CarouselRepresentable(model: model)
+                        .frame(height: model.height + shadowBottomPadding)
+                        .opacity(debug ? 0.75 : 1)
+                        .onAppear{
+                            model.scrollToMiddle()
+                            //                    model.startTimer()
+                            model.configureScrollObserver()
+                        }
+                    
+                    Text("Bottom")
+                    
+                    ForEach(0..<100) {_ in
+                        Color.orange
+                            .frame(height: 100)
+                            .cornerRadius(10)
+                            .padding(.horizontal, spacing)
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem {
+                    Toggle("Debug", isOn: $debug.animation())
+                        .toggleStyle(.switch)
+                }
             }
         }
     }
