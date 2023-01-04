10246f5baf1ffe942c5831b7dfdc47b3c8a93705
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-01-04 15:48:37 -0500

    added example

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blur = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text(blur.formatted())
+            Slider(value: $blur, in: 0...20)
+            
+            Image("dog")
+                .resizable()
+//                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                .blur(radius: blur)
+                .border(Color.black)
+        }
     }
 }
 
