ae07dc1e1426f6a4c9bab74a57f144e8f99dbb2e
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-04-13 16:45:10 -0400

    testing

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab: DiscoverTab = .offers
+    @Namespace private var tabIndicatorAnimation
+    @State private var width: CGFloat = 64
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(alignment: .top, spacing: 12) {
+            ForEach(DiscoverTab.allCases) { tab in
+                VStack(spacing: 10) {
+                    Image(systemName: tab.assetTitle(isSelected: selectedTab == tab))
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .onTapGesture {
+                            withAnimation(.linear(duration: 0.1)) {
+                                selectedTab = tab
+                            }
+                        }
+
+                    if selectedTab == tab {
+                        Capsule()
+                            .frame(height: 2)
+                            .matchedGeometryEffect(id: "TabIndicator", in: tabIndicatorAnimation)
+                            .foregroundColor(Color.blue)
+                    }
+                }
+                .frame(width: width)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
     }
 }
 
