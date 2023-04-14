7f78a62eb3b6a7cc7bd328b68dd5e76098f324e0
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-04-14 16:20:55 -0400

    testing

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,31 @@
 //
 
 import SwiftUI
+import FLAnimatedImage
 
 struct ContentView: View {
+    let localImage = FLAnimatedImage(gifData: gifData)!
+    @State private var image: FLAnimatedImage?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            GIFView(image: localImage)
+                .frame(width: 200, height: 100)
+            
+            if let image {
+                GIFView(image: image)
+                    .frame(width: 200, height: 100)
+            }
+        }
+        .task {
+            Task(priority: .userInitiated) {
+                let url = URL(string: "https://cdn.staging-images.fetchrewards.com/carousels/38376a7ce028e5ba94fb51c85ced5e8f13c47399.gif")!
+                do {
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    image = .init(gifData: data)
+                } catch {}
+            }
+        }
     }
 }
 
