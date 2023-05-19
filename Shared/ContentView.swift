0aaee440ca5033d6978541a0570d7ad4581b0733
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-05-19 19:07:22 -0400

    testing

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: UIImage?
+    let images = [
+        "screenshot1",
+        "screenshot2",
+        "screenshot3",
+        "screenshot4",
+        "screenshot5",
+    ].compactMap{
+        let image = UIImage(named: $0)
+        return image?.scaled(to: 0.1)
+    }
+    
+    let resized1 = UIImage(named: "screenshot1")!.scaled(to: 0.6)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Image(uiImage: resized1)
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("screenshot1")
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            if let image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                ProgressView()
+                    .onAppear {
+                        print(images.map(\.size.width).reduce(0, +))
+                        image = images.stitchImages(isVertical: false, spacing: 50)
+                    }
+            }
+        }
     }
 }
 
