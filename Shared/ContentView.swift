bfa559c8941abcda55da01e242f1e4102964930d
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-06-15 15:38:25 -0400

    testing

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var id: Int?
+    let array = Array(0..<1_000)
+    let colors = [
+        Color.blue,
+        Color.red,
+        Color.orange,
+        Color.purple
+    ]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("ID: \(id?.formatted() ?? "None")")
+                .onTapGesture {
+                    id = 0
+                }
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 0) {
+                    ForEach(array, id: \.self) { i in
+                        colors[i % 4]
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                            .aspectRatio(16.0 / 9.0, contentMode: .fit)
+                            .containerRelativeFrame(.horizontal)
+                            .id(i)
+                            .scrollTransition(axis: .horizontal) { content, phase in
+                                content
+                                    .scaleEffect(phase.isIdentity ? 1 : 0.75)
+                            }
+                    }
+                }
+                .scrollTargetLayout()
+            }
+            .scrollPosition(id: $id)
+            .scrollTargetBehavior(.paging)
+            .onAppear {
+                id = array.count / 2
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
