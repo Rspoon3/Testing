5cfcaf3b72ff107cedb59d0c11679b9caec8d203
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-06-22 10:49:22 -0400

    testing

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -16,37 +16,51 @@ struct ContentView: View {
         Color.orange,
         Color.purple
     ]
+    @State var x = 0.0
 
     var body: some View {
         VStack {
             Text("ID: \(id?.formatted() ?? "None")")
                 .onTapGesture {
-                    id = 0
+                    withAnimation {
+                        id = 0
+                    }
                 }
             
             ScrollView(.horizontal, showsIndicators: false) {
-                LazyHStack(spacing: 0) {
+                LazyHStack(spacing: -100) {
                     ForEach(array, id: \.self) { i in
                         colors[i % 4]
                             .cornerRadius(10)
-                            .padding(.horizontal)
                             .aspectRatio(16.0 / 9.0, contentMode: .fit)
-                            .containerRelativeFrame(.horizontal)
-                            .id(i)
-                            .scrollTransition(axis: .horizontal) { content, phase in
+                            .containerRelativeFrame(.horizontal, count: 10, span: 8, spacing: 0)
+                            .visualEffect { content, proxy in
                                 content
-                                    .scaleEffect(phase.isIdentity ? 1 : 0.75)
+                                    .scaleEffect(size(using: proxy))
                             }
+                           
                     }
                 }
                 .scrollTargetLayout()
             }
             .scrollPosition(id: $id)
-            .scrollTargetBehavior(.paging)
+//            .scrollTargetBehavior(.paging)
             .onAppear {
                 id = array.count / 2
             }
         }
+    }
+    
+    func size(using proxy: GeometryProxy) -> Double {
+        let scrollViewWidth = proxy.bounds(of: .scrollView)!.width
+        let position = proxy.frame(in: .scrollView).midX
+        let distanceFromCenter = abs(scrollViewWidth / 2 - position)
+        
+        
+        let value = 1 - distanceFromCenter / scrollViewWidth
+        print(scrollViewWidth, position, distanceFromCenter, value)
+        
+        return value
     }
 }
 
