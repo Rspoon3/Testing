d56b48f2857fc1e4ca87ca25119bf7f54521c047
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-12-07 11:40:31 -0500

    Better example for Stack overflow

--- Shared/CarouselView.swift
+++ Shared/CarouselView.swift
@@ -6,45 +6,47 @@
 //
 
 import SwiftUI
-import SwiftUIIntrospect
 
-@MainActor
 struct CarouselView: View {
     @StateObject private var viewModel = CarouselViewModel()
     private let colors: [Color] = [.teal, .blue, .green, .orange, .purple, .red]
-
+    
     var body: some View {
-        VStack {
-            GeometryReader { proxy in
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHStack {
-                        ForEach(0..<viewModel.maxValue, id: \.self) { i in
-                            let v = i % colors.count
-                            colors[v]
-                                .clipShape(RoundedRectangle(cornerRadius: 12))
-                                .aspectRatio(16.0 / 9.0, contentMode: .fit)
-                                .frame(width: proxy.size.width - viewModel.safeAreaPadding * 2)
-                                .overlay {
-                                    Text(i.formatted())
-                                        .font(.largeTitle)
-                                }
-                                .visualEffect { [viewModel] content, geometryProxy in
-                                    content
-                                        .scaleEffect(
-                                            y: viewModel.yScale(using: geometryProxy)
-                                        )
-                                }
-                        }
+        GeometryReader { proxy in
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: viewModel.spacing) {
+                    ForEach(0..<viewModel.maxValue, id: \.self) { i in
+                        let v = i % colors.count
+                        colors[v]
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .aspectRatio(2, contentMode: .fit)
+                            .frame(width: proxy.size.width - viewModel.spacing * 4)
+                            .overlay {
+                                Text(i.formatted())
+                                    .font(.largeTitle)
+                            }
+                            .visualEffect { [viewModel] content, geometryProxy in
+                                content
+                                    .scaleEffect(
+                                        y: viewModel.yScale(using: geometryProxy)
+                                    )
+                            }
                     }
-                    .scrollTargetLayout()
                 }
-                .scrollTargetBehavior(.viewAligned)
-                .safeAreaPadding(.horizontal, viewModel.safeAreaPadding)
-                .scrollPosition(id: $viewModel.id)
-                .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16, .v17)) { scrollView in
-                    viewModel.set(scrollView)
-                }
+                .scrollTargetLayout()
             }
+            .scrollTargetBehavior(.viewAligned)
+            .safeAreaPadding(.horizontal, viewModel.spacing * 2)
+            .scrollPosition(id: $viewModel.id)
+//            .onAppear {
+//                viewModel.id = 25
+//            }
+//            .task {
+//                try? await Task.sleep(for: .seconds(1))
+//                withAnimation {
+//                    viewModel.id = 250
+//                }
+//            }
         }
     }
 }
--- Shared/CarouselViewModel.swift
+++ Shared/CarouselViewModel.swift
@@ -6,43 +6,13 @@
 //
 
 import SwiftUI
-import Clocks
 
 @MainActor
 final class CarouselViewModel: ObservableObject {
-    @Published var id: Int? //= 500
-    let maxValue = 50_000
+    @Published var id: Int? // = 250
+    let maxValue = 500
     let scale: (max: CGFloat, min: CGFloat) = (1, 0.8)
-    let safeAreaPadding: CGFloat = 16
-    let clock: any Clock<Duration>
-    var timerTask: Task<Void, Error>?
-
-    // MARK: - Initializer
-    
-    init(clock: any Clock<Duration> = ContinuousClock()) {
-        self.clock = clock
-        self.timerTask = Task {
-//            for await _ in self.clock.timer(interval: .seconds(3)) {
-//                withAnimation {
-//                    id = (id ?? 0) + 1
-//                }
-//            }
-        }
-    }
-    
-    // MARK: - Private Helpers
-    
-    @objc private func gestureRecognizerUpdate(panGesture: UIPanGestureRecognizer) {
-        guard panGesture.state == .began else { return }
-        self.timerTask?.cancel()
-        self.timerTask = nil
-    }
-    
-    // MARK: - Public Helpers
-    
-    func set(_ scrollView: UIScrollView) {
-        scrollView.panGestureRecognizer.addTarget(self, action: #selector(gestureRecognizerUpdate))
-    }
+    let spacing: CGFloat = 12
     
     nonisolated func yScale(using proxy: GeometryProxy) -> Double {
         let itemMidX = proxy.frame(in: .scrollView).midX
@@ -54,7 +24,7 @@ final class CarouselViewModel: ObservableObject {
         let scrollViewMidX = scrollViewWidth / 2
         let distanceFromCenter = abs(scrollViewMidX - itemMidX)
         let itemWidth = proxy.size.width
-        let percentageToMidX = 1 - (distanceFromCenter / (itemWidth - safeAreaPadding))
+        let percentageToMidX = 1 - (distanceFromCenter / (itemWidth - spacing))
         let calculatedScale = ((scale.max - scale.min) * percentageToMidX) + scale.min
         return max(scale.min, calculatedScale)
     }
