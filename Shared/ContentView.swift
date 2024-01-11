0eb7b2df3704d3f62558a1a52b9c2a28ff20c760
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2024-01-11 13:00:02 -0500

    testing

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,59 @@
 //
 
 import SwiftUI
+import SwiftCSV
 
 struct ContentView: View {
+    @State var dictionary: [Int: Int] = [:]
+    let minimumiOSVersion = 15
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onAppear {
+                test()
+            }
+    }
+    
+    func test() {
+        do {
+            let csv = try EnumeratedCSV(name: "versions", extension: "csv", bundle: .main)!
+            let allColumns = csv.columns!
+            let iOSColumns = allColumns.filter {$0.header.contains("iOS")}
+            
+            for column in iOSColumns {
+                let iOSVersionString = column.header.components(separatedBy: CharacterSet.decimalDigits.inverted).filter{!$0.isEmpty}.joined(separator: ".")
+                let iOSVersion = Double(iOSVersionString)!
+                let majorVersion = Int(iOSVersion)
+                
+                print(column.header, iOSVersion, majorVersion)
+                
+                if majorVersion < minimumiOSVersion { continue }
+                
+                
+                for row in column.rows.suffix(1) {
+                    let rowValue = Int(Double(row) ?? 0)
+                    let existingValue = dictionary[majorVersion] ?? 0
+                    let newValue = rowValue + existingValue
+                    
+                    if iOSVersion == 17.2 {
+                        print(newValue)
+                    }
+                    dictionary.updateValue(newValue, forKey: majorVersion)
+                }
+            }
+            
+            print(dictionary)
+            
+            let total = dictionary.map(\.value).reduce(0, +)
+            
+            for (key, value) in dictionary {
+                let percent = Double(value) / Double(total)
+                print(key, percent.formatted(.percent))
+            }
+        } catch {
+            print("Error")
+        }
     }
 }
 
