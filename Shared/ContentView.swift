0ebbf50b615377fac49788c8db50e704b76c8bb3
Ricky <42879920+Rspoon3@users.noreply.github.com>
2024-09-18 11:07:00 -0400

    Added example

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,10 +7,38 @@
 
 import SwiftUI
 
+public enum Tab {
+    case home, settings
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State public var selectedTab: Tab = .home
+    @State private var test = false
+    
+    // MARK: - Initializer
+    
+    public init() {}
+    
+    // MARK: - Body
+    
+    public var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Home")
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                        .accessibility(label: Text("Home"))
+                }
+                .tag(Tab.home)
+            
+            Form {
+                Toggle("Automatically save to files", isOn: $test)
+            }
+            .tabItem {
+                Label("Settings", systemImage: "gear")
+                    .accessibility(label: Text("Settings"))
+            }
+            .tag(Tab.settings)
+        }
     }
 }
 
