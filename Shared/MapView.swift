edb78e91c60429f38d9f4b10b88eb65303b5ecc3
Ricky Witherspoon <42879920+Rspoon3@users.noreply.github.com>
2024-12-01 01:28:57 -0500

    location button

--- Shared/MapView.swift
+++ Shared/MapView.swift
@@ -8,31 +8,15 @@
 import SwiftUI
 import MapKit
 
-import SwiftUI
-import MapKit
-
 struct MapView: View {
-    // Define regions for 5 random state capitals
-    let stateCapitals = [
-        (name: "Montgomery, AL", coordinate: CLLocationCoordinate2D(latitude: 32.3792, longitude: -86.3077)),
-        (name: "Juneau, AK", coordinate: CLLocationCoordinate2D(latitude: 58.3019, longitude: -134.4197)),
-        (name: "Phoenix, AZ", coordinate: CLLocationCoordinate2D(latitude: 33.4484, longitude: -112.0740)),
-        (name: "Little Rock, AR", coordinate: CLLocationCoordinate2D(latitude: 34.7465, longitude: -92.2896)),
-        (name: "Sacramento, CA", coordinate: CLLocationCoordinate2D(latitude: 38.5758, longitude: -121.4789))
-    ]
-    
-    let region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 37.0902, longitude: -95.7129), // Center of the US
-        span: MKCoordinateSpan(latitudeDelta: 50, longitudeDelta: 50) // Broad view for multiple states
-    )
-    
+    @Namespace var mapScope
     let states = Bundle.main.decode(
         [USState].self,
         from: "states.json"
     )
     
     var body: some View {
-        Map {
+        Map(scope: mapScope) {
             // Add annotations for each state capital
             ForEach(states) { state in
                 Annotation(
@@ -48,7 +32,9 @@ struct MapView: View {
                 }
             }
         }
-        .ignoresSafeArea(edges: .top)
+        .mapControls {
+            MapUserLocationButton(scope: mapScope)
+        }
     }
 }
 
