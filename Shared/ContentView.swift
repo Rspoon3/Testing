d553a0a46d2a7cb88c8b13c3c0f40aeeed43eb71
Ricky Witherspoon <42879920+Rspoon3@users.noreply.github.com>
2025-03-27 21:41:33 -0400

    Feedback example FB17031212

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,14 +7,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
+public enum Tab {
+    case main
+}
+
+struct AppTabNavigation: View {
+    @State private var tabSelection = Tab.main
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $tabSelection) {
+            NavigationStack { // Changed to NavigationView to work
+                ContentView()
+            }
+            .tag(Tab.main)
+            .tabItem {
+                Label("Main", systemImage: "star")
+            }
+        }
     }
 }
 
+struct ContentView: View {
+    @State private var searchText = ""
+    
+    var body: some View {
+        List(0..<100) { i in
+            NavigationLink {
+                DetailsView(i: i)
+            } label: {
+                Text("Select \(i)")
+            }
+        }
+        .navigationTitle("Main")
+        .searchable(text: $searchText)
+    }
+}
 
-#Preview {
-    ContentView()
+struct DetailsView: View {
+    @State private var searchText = ""
+    let i: Int
+    
+    // MARK: - Body
+    
+    var body: some View {
+        List {
+            ForEach(0..<10, id: \.self) { i in
+                Text("Hello \(i)")
+            }
+        }
+        .navigationTitle(i.formatted())
+        .searchable(text: $searchText)
+    }
 }
